896eb895e91e29f69e54d5711b60a7db64e1e4e6
Arbab Husain <arbabhusain200@gmail.com>
2024-08-08 20:09:29 -0400

    Added voice input to llama

--- API/LLama/llamabot/src/App.jsx
+++ API/LLama/llamabot/src/App.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import "./style.css"
 import { useState } from 'react'
 import { getLlamaResponse } from './LlamaEngine';
-
+import {  } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faChevronRight} from '@fortawesome/free-solid-svg-icons'
+import { faMicrophone } from '@fortawesome/free-solid-svg-icons';
 
 export default function App(){
     const [messages, setMessages] = useState([]);
@@ -51,7 +54,27 @@ export default function App(){
     };
 
     const handleVoice = () => {
-        //Integrate voice
+        const recognition = new window.webkitSpeechRecognition();
+        recognition.continuous = false; 
+        recognition.interimResults = false; 
+        recognition.lang = 'en-US';
+
+        recognition.start();
+
+        recognition.onresult = (event) => {
+            const transcript = event.results[0][0].transcript;
+            setInput(transcript); 
+            handleSend(); 
+        };
+
+        recognition.onerror = (event) => {
+            console.error("Speech recognition error: ", event.error);
+            alert('Speech recognition error occurred. Please try again.');
+        };
+    
+        recognition.onend = () => {
+            console.log("Speech recognition ended.");
+        };
     };
     
     return(
@@ -71,8 +94,12 @@ export default function App(){
                     onKeyPress={handleKeyPress}
                     placeholder="Type a message..."
                 />
-                <button className="btn-send" onClick={handleSend}>Send</button>
-                <button className="btn-voice" onClick={handleVoice}>Voice</button>
+                <button className="btn-send" onClick={handleSend}>
+                    <FontAwesomeIcon icon={faChevronRight} />
+                </button>
+                <button className="btn-voice" onClick={handleVoice}>
+                    <FontAwesomeIcon icon={faMicrophone} />
+                </button>
             </div>
         </div>
     )
